04120fd278906de008d12df6526dc99c3c84a623
iritaa <irita1909@gmail.com>
2019-09-16 15:36:28 -0400

    Update run_ciftify.sh

--- run_ciftify.sh
+++ run_ciftify.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
-export SOURCE_INPUT=$1
-export OUTPUT_CIFTIFY=$2
-##Input and Output  Folder
+export SOURCE_INPUT=$1   ## Input Folder
+export OUTPUT_CIFTIFY=$2 ## Output Folder
 
-
-##Verify
+## Verify the file exist
 
 export file="subjects.tsv"
 
 if [ -f "$file" ]
 then
-	echo "$file found."
+	echo "$file exist."
 	else
-		
 		echo "$file not found."
 		ls $SOURCE_INPUT/ >> $file
 fi
-
 while read participant
-
 do
 ciftify_recon_all --verbose --surf-reg FS --fs-subjects-dir $SOURCE_INPUT  --ciftify-work-dir $OUTPUT_CIFTIFY $participant
-
 done  < $file
-
 ##Delete the file" 
 rm -rf $file
- 
 echo -e "Done CiftiFy Processing"

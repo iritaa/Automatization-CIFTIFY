7e689cd666b71e5b23e7139f24d6c6263c3f96e0
iritaa <irita1909@gmail.com>
2019-09-16 15:23:00 -0400

    Add files via upload

--- run_ciftify.sh
+++ run_ciftify.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
-### Script desarrollado por Iris Rodriguez Gil. CNEURO. Septiembre 2019
 
 export SOURCE_INPUT=$1
 export OUTPUT_CIFTIFY=$2
-## Folder donde se encuentran los sujetos
+##Input and Output  Folder
 
-## subjects.tsv: file que contiene los nombres de los folders asociados a 
-##los sujetos.
-##EL nombre del folder, debera estar asociado al nombre del sujeto
 
+##Verify
+
+export file="subjects.tsv"
+
+if [ -f "$file" ]
+then
+	echo "$file found."
+	else
+		
+		echo "$file not found."
+		ls $SOURCE_INPUT/ >> $file
+fi
 
 while read participant
 
 do
-echo $participant
-
 ciftify_recon_all --verbose --surf-reg FS --fs-subjects-dir $SOURCE_INPUT  --ciftify-work-dir $OUTPUT_CIFTIFY $participant
 
+done  < $file
 
-done  < "subjects.tsv"
-
-echo -e "Done"
+##Delete the file" 
+rm -rf $file
+ 
+echo -e "Done CiftiFy Processing"
